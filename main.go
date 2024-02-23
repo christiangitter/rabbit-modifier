8ba59e009a419834892a63d876c4a0be0771b3ba
Christian Gitter <gitter.christian@yahoo.de>
2024-02-23 22:30:59 +0100

    connect to queue

--- main.go
+++ main.go
@@ -1,10 +1,42 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+)
 
 // simple error handling helper function
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	// Connect to RabbitMQ server
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	// Create a channel
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	// register a consumer
+	msgs, err := ch.Consume(
+		"queue_name", // queue
+		"",           // consumer
+		true,         // auto-ack
+		false,        // exclusive
+		false,        // no-local
+		false,        // no-wait
+		nil,          // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	for msg := range msgs {
+		log.Printf("Received a message: %s", msg.Body)
 	}
 }
